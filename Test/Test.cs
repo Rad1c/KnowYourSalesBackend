59e32586793d642073e2368c6b327cad39dc3ac9
Aleksandar Radc <aleksandar.radic@lanaco.com>
2023-07-16 23:21:04 +0200

    test

---
+++ Test/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace Test
+{
+    public class Test
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Test()
+        {
+
+        }
+    }
+}
